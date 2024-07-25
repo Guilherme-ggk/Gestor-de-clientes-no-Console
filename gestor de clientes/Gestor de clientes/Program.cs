520a34c37c1515b13bc9025ff2286e6dd3eb1cfd
Gui-gt <ggk0830755@gmail.com>
2024-07-24 23:07:17 -0300

    atualizando meu git hub
    colocando um novo projeto

---
+++ gestor de clientes/Gestor de clientes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestor_de_clientes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ gestor de clientes/Tipos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tipos em C sharp
+            /*
+             
+            int = 20 - Int armazena números inteiros
+            float = 2,0 - Float armazena números flutuantes 
+            bool = true or false - Bool armazena condições verdadeiras ou falsas
+            string = Guilherme - String armazena textos, nomes
+            char = G - Char é armazena uma letra, um nome é igual o número de caracteres que ele possui
+             
+             */
+        }
+    }
+}
