a24cd59a57a3c7d1bf53bf5bc1e8ed65f551b604
Gui-gt <ggk0830755@gmail.com>
2024-07-25 23:52:31 -0300

    atualizações de c#

--- gestor de clientes/Gestor de clientes/Program.cs
+++ gestor de clientes/Gestor de clientes/Program.cs
@@ -9,7 +9,7 @@ namespace Gestor_de_clientes
 {
     internal class Program
     {
-        
+        [System.Serialization]
         struct Cliente
         {
             public string nome;
@@ -17,21 +17,74 @@ namespace Gestor_de_clientes
             public string cpf;
         }
 
+        static List<Cliente> clientes = new List<Cliente>();
+
         enum Menu {listagem = 1, adicionar, remover, sair};
 
         static void Main(string[] args)
         {
-            Console.WriteLine("<<  Gestor de Clientes  >>");
+                Carregar();
+
+            bool escolheuSair = false;
+            while(!escolheuSair)
+            {
+                Console.WriteLine("<<  Gestor de Clientes  >>");
             Console.WriteLine("1-listagem\n2-adicionar\n3-remover\n4-sair");
             int intop = int.Parse(Console.ReadLine());
             Menu opcao = (Menu) opcao;
 
             switch (opcao)
             {
-                case Menu.
+                case Menu.listagem;
+                    break;
+                case Menu.adicionar;
+                    break;
+                case Menu.remover;
+                    break;
+                case Menu.sair;
+                        escolheuSair = true;
+                    break;
                 
             }
+                Console.ReadLine();
+            }
+        }
+        static void Adicionar()
+        {
+                Cliente cliente = new Cliente();
 
+                Console.WriteLine("Adicionando Clientes");
+
+                Console.WriteLine("Nome:");
+                cliente.nome = Console.ReadLine();
+
+                Console.WriteLine("Email:");
+                cliente.email = Console.ReadLine();
+
+                Console.WriteLine("CPF:");
+                cliente.cpf = Console.ReadLine();
+
+                clientes.Add(cliente);
+                Salvar();
+
+                Console.WriteLine("Cliente adicionado com sucesso!");
+                Console.WriteLine("aperte enter para voltar ao menu.");
+        }
+        static void Listagem()
+        {
+
+        }
+        static void Remover()
+        {
+
+        }
+        static void Salvar()
+        {
+
+        }
+        static void Carregar()
+        {
+            
         }
     }
 }
