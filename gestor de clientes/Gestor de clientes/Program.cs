31738ab7fe4ad7ea5afde20b7c0d6f68b31e5d78
Gui-gt <ggk0830755@gmail.com>
2024-07-25 23:33:40 -0300

    atualizando meus exercicios
    estudando mais c#

--- gestor de clientes/Gestor de clientes/Program.cs
+++ gestor de clientes/Gestor de clientes/Program.cs
@@ -8,8 +8,21 @@ namespace Gestor_de_clientes
 {
     internal class Program
     {
+        
+
         static void Main(string[] args)
         {
+            Console.WriteLine("<<  Gestor de Clientes  >>");
+            Console.WriteLine("1-listagem\n2-adicionar\n3-remover\n4-sair");
+            int intop = int.Parse(Console.ReadLine());
+            Menu opcao = (Menu) opcao;
+
+            switch (opcao)
+            {
+                case Menu.
+                
+            }
+
         }
     }
 }
